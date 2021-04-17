44909ca7593b10e24ba48d656ef6cb0300964337
reitmas32 <rafa.zamora.rals@gmail.com>
2021-04-17 07:19:20 -0500

    feat(cmake): Example update cmake

--- example/ExampleMoonTerminal/cmake/moon_terminal.cmake
+++ example/ExampleMoonTerminal/cmake/moon_terminal.cmake
@@ -8,6 +8,11 @@ set(MOON_TERMINAL_INCLUDE_DIR
 ${CMAKE_SOURCE_DIR}/vendor
 ${CMAKE_SOURCE_DIR}/vendor/Moon_Terminal/include )
 
-set(MOON_TERMINAL_LIBRARIES ncurses)
-
+option(LOGS "ON")
+if(${LOGS} STREQUAL "OFF")
+    add_compile_definitions(RELEASE)
+    set(MOON_TERMINAL_LIBRARIES ${CMAKE_SOURCE_DIR}/vendor/Moon_Terminal/build/libmoon_terminal.a ncurses)
+else()
+    set(MOON_TERMINAL_LIBRARIES ${CMAKE_SOURCE_DIR}/vendor/Moon_Terminal/build/libmoon_terminal.a ncurses)
+endif()
 endif()
